7e5ca7c3f0183720ed49df85c8f1099e825e196a
prrateekk <prrateekk@hotmail.com>
2016-05-25 19:34:35 +0530

    Added Private Messaging

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
-	nicknames = [];
+	users = {};
 
 server.listen(3000);
 
@@ -12,28 +12,46 @@ app.get('/',function(req,res){
 
 io.sockets.on('connection',function(socket){
 	socket.on('new user',function(data,callback){
-		if (nicknames.indexOf(data)!=-1){
+		if (data in users){
 			callback(false);
 		}
 		else{
 			callback(true);
 			socket.nickname = data;	//property of a socket
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
 			updateNickNames();
 		}
 	});
 
 	function updateNickNames(){
-		io.sockets.emit('usernames',nicknames);
+		io.sockets.emit('usernames',Object.keys(users)) ;
 	}
 
 	socket.on('send-message',function(data){
-		io.sockets.emit('new message',{msg:data,nick:socket.nickname});
+		var msg = data.trim();
+		if (msg.substr(0,2)==='//'){
+			msg = msg.substr(2);
+			var ind = msg.indexOf(' ');
+			if (ind!=-1){
+				var name = msg.substr(0,ind);
+				msg = msg.substr(ind+1);
+				//console.log(msg);
+				if (name in users && name!=socket.nickname){
+					console.log('Private Message.');
+					users[name].emit('private message',{msg:msg,nick:socket.nickname});
+					socket.emit('private message',{msg:msg,nick:socket.nickname});
+				}
+				else console.log('Error!');
+			}
+			else console.log('Error!');
+		}
+		else{
+			io.sockets.emit('new message',{msg:data,nick:socket.nickname});
+		}
 	});
 
 	socket.on('disconnect',function(data){
-		nicknames.splice(nicknames.indexOf(socket.nickname),1);	
+		delete users[socket.nickname];
 		updateNickNames();
 	});
-
 });
